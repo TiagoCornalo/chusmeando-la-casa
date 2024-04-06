0ec759cf6e5c3c4dfa4b740bcc29f5fc1820216e
TiagoCornalo <cornalotiago@gmail.com>
2024-04-06 17:17:56 -0300

    feat(general): :sparkles: added prettier and eslint config, also added some shadcn components

---
+++ src/components/index.ts
@@ -0,0 +1,3 @@
+export {default as ModeToggle} from './mode-toggle/ModeToggle.vue';
+export * from './ui/button';
+export * from './ui/dropdown-menu';
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,13 +2,12 @@
   <div>
     <h1>Home</h1>
     <p>Welcome to the Home page!</p>
+    <ModeToggle />
   </div>
 </template>
 
 <script setup lang="ts">
-
+import { ModeToggle } from '@/components'
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
