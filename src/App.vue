b846f3ba239eca1bd35060e10146d571503da300
TiagoCornalo <cornalotiago@gmail.com>
2024-04-06 16:48:42 -0300

    fix(config-files): :fire: fixed files extension

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
-<script setup lang="ts">
-</script>
-
 <template>
   <RouterView />
 </template>
+
+<script setup lang="ts">
+</script>
 
 <style scoped>
 </style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -3,7 +3,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 const routes = [
   {
     path: '/',
-    component: () => {}
+    component: () => import ('@/views/HomeView.vue')
   }
 ]
 
